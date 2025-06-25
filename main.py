f158d8fcb096b7e816e87e7e414ea1fc4c5420e9
Nikhil Saji <nikhilsaji200@gmail.com>
2025-06-26 03:52:40 +0530

    Updatedt yaml file

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from tagging import auto_tag_recipes, tag_recipe_by_id
 
 
 if __name__ == "__main__":
-    
+    print("Starting recipe tagging...")
     # tag all recipes based on keywords
     auto_tag_recipes()
