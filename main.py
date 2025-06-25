bc0aca7f9ea986b4abd09c75f258b938983307cd
Nikhil Saji <nikhilsaji200@gmail.com>
2025-06-26 05:25:02 +0530

    added timer

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from tagging import auto_tag_recipes,bulk_insert_keywords
+import time
 
 
 if __name__ == "__main__":
+    start_time = time.time()
+
     bulk_insert_keywords()
     auto_tag_recipes()
+    
+    end_time = time.time()
+    print(f"Total time taken: {end_time - start_time} seconds")
